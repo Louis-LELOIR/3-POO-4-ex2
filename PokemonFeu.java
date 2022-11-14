107f4628c26aa39965837d4eb0c35e9d0a117a0f
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-14 10:59:57 +0000

    Add

--- PokemonFeu.java
+++ PokemonFeu.java
@@ -1,3 +1,14 @@
 public class PokemonFeu extends Pokemon {
-  
+
+  public PokemonFeu(String nom, int pv, String atk){
+    super(nom, pv, atk);
+  }
+
+  @Override
+  public void attaquer(Pokemon p){
+    switch(p) {
+      case insta
+    }
+    p.setPv(this.atk);
+  }
 }
