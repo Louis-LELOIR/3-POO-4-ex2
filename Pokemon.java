a6fb57df8cca16a8f79cdb75e7fead4f37a81a25
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-14 10:54:52 +0000

    Adding information on Pokemon Class

--- Pokemon.java
+++ Pokemon.java
@@ -2,7 +2,7 @@ public class Pokemon {
 
   private String nom;
   private int pv;
-  private String atk;
+  private int atk;
 
   public Pokemon(String nom, int pv, String atk){
     this.nom = nom;
@@ -16,7 +16,23 @@ public class Pokemon {
   public int getPv(){
     return this.pv;
   }
-  public String getAtk(){
+  public int getAtk(){
     return this.atk;
+  }
+  public void setPv(int atk){
+    this.pv -= atk;
+  }
+
+  public boolean estKo() {
+    return (pv === 0) ? true: false;
+  }
+
+  public void attaquer(Pokemon p){
+    p.setPv(this.atk);
+  }
+
+  @Override
+  public String toString(){
+    return "Nom : " + this.getNom() + " PV : " + this.getPv() + " Puissance attaque : " + this.getAtk();
   }
 }
